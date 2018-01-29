9a8cee31444628c9f45ca0606db6b4cc58571b26
José Medrano <jose.medrano@xamarin.com>
2018-01-29 06:11:01 +0100

    Adds Broadlink Server and some extension methods

---
+++ Broadlink.NET/BroadLinkService.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Broadlink.NET
+{
+	public class BroadLinkService : IDisposable
+	{
+		const int DefaultWaitTime = 25;
+
+		Client discoverClient;
+		RMDevice rmDevice;
+
+		public BroadLinkService()
+		{
+			discoverClient = new Client();
+		}
+
+		public RMDevice DiscoverDevice()
+		{
+			RMDevice discovered = null;
+			EventHandler<BroadlinkDevice> handler = (s, e) =>
+			{
+				discovered = e as RMDevice;
+			};
+
+			discoverClient.DeviceHandler += handler;
+
+			discoverClient.DiscoverAsync().Wait();
+
+			for (int i = 0; i < DefaultWaitTime; i++)
+			{
+				if (discovered != null)
+					return discovered;
+				Task.Delay(1000).Wait();
+			}
+
+			discoverClient.DeviceHandler -= handler;
+
+			return discovered;
+		}
+
+		public void Dispose()
+		{
+			discoverClient.Dispose();
+		}
+
+		public async Task Search()
+		{
+			EventHandler<BroadlinkDevice> handler = (s, e) =>
+			{
+				rmDevice = e as RMDevice;
+			};
+
+			discoverClient.DeviceHandler += handler;
+
+			rmDevice = null;
+			discoverClient.DiscoverAsync().Wait();
+
+			for (int i = 0; i < DefaultWaitTime && rmDevice == null; i++)
+			{
+				await Task.Delay(1000);
+			}
+
+			discoverClient.DeviceHandler -= handler;
+		}
+	}
+}
---
+++ Broadlink.NET/RMDeviceExtensions.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Broadlink.NET
+{
+	public static class RMDeviceExtensions
+	{
+		public static void SendCommand(this RMDevice sender, string command)
+		{
+			sender.SendRemoteCommandAsync(command.HexToBytes()).Wait();
+		}
+
+		public static RMDevice WaitUntilReady(this RMDevice device)
+		{
+			if (!device.IsEventsReady)
+			{
+				bool ready = false;
+
+				EventHandler handler = (sen, er) =>
+				{
+					ready = true;
+				};
+
+				device.OnDeviceReady += handler;
+				//e.OnTemperature += RMDevice_OnTemperature;
+				//e.OnRawData += RMDevice_OnRawData;
+				//e.OnRawRFDataFirst += RMDevice_OnRawRFDataFirst;
+				//e.OnRawRFDataSecond += RMDevice_OnRawRFDataSecond;
+				//e.OnSentDataCallback += RMDevice_OnSentDataCallback;
+				device.IsEventsReady = true;
+				device.AuthorizeAsync().Wait();
+
+				for (int i = 0; i < 20 && !ready; i++)
+				{
+					Task.Delay(1000).Wait();
+				}
+
+				device.OnDeviceReady -= handler;
+
+				if (!ready)
+				{
+					Console.WriteLine("Device was not ready in the requested time");
+				}
+			}
+			return device;
+		}
+	}
+}
